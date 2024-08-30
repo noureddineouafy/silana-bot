d18960e77be22a4c6dc45c700f6b0fa2bf131228
noureddineouafy <130229691+noureddineouafy@users.noreply.github.com>
2024-08-30 02:42:18 +0100

    Create githubsearch.js

---
+++ plugins/githubsearch.js
@@ -0,0 +1,41 @@
+import fetch from 'node-fetch'
+let handler = async (m, { text, command, usedPrefix }) => {
+    if (!text) throw 'البحث عن مشاريع في github مثال :\n\n*.githubsearch bobiza*'
+    let res = await fetch(global.API('https://api.github.com', '/search/repositories', {
+        q: text
+    }))
+    let json = await res.json()
+    if (res.status !== 200) throw json
+    let str = json.items.map((repo, index) => {
+        return `
+${1 + index}. *${repo.full_name}*${repo.fork ? ' (fork)' : ''}
+_${repo.html_url}_
+_Made by\n *${formatDate(repo.created_at)}*_
+_Lastupdateon\n
+*${formatDate(repo.updated_at)}*_
+👁  ${repo.watchers}   🍴  ${repo.forks}   ⭐  ${repo.stargazers_count}
+${repo.open_issues} Issue${repo.description ? `
+*Description:*\n${repo.description}` : ''}
+*Clone:* \`\`\`$ git clone ${repo.clone_url}\`\`\`
+`.trim()
+    }).join('\n\n')
+    m.reply(str)
+}
+handler.help = ['githubsearch']
+handler.tags = ['search']
+handler.command =/^githubsearch$/i
+
+export default handler
+
+function formatDate(n, locale = 'id') {
+    let d = new Date(n)
+    return d.toLocaleDateString(locale, {
+      weekday: 'long',
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric'
+    })
+  }
